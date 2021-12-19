1687e47d43d2e8455572cf20c225bc0946bf9924
pobydoby <pabitra.aryanz@gmail.com>
2021-12-19 00:42:45 -0600

    templates, layouts for the ToDo List

--- app.js
+++ app.js
@@ -1,39 +1,83 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 const app = express();
+
+var items = ["Do the dishes"];
+var workItems = [];
 
 // embedded javascript templates defination
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", function (req, res) {
-	
-	var today = new Date();
-	var currentDay = today.getDay();
-	var day = "";
-	console.log(currentDay);
-	if (currentDay === 6 || currentDay === 0) {
-		day = "weekend";
-		// res.sendFile(__dirname + "/weekend.html");
-	} else {
-		day = "weekday";
-		// res.sendFile(__dirname + "/weekday.html");
-	}
+app.use(express.static("public"));
+app.get("/", function(req, res) {
 
-	switch (currentDay) {
-		case 0: day = "Sunday"; break;
-		case 1: day = "Monday"; break;
-		case 2: day = "Tuesday"; break;
-		case 3: day = "Wednesday"; break;
-		case 4: day = "Thursday"; break;
-		case 5: day = "Friday"; break;
-		case 6: day = "Saturday"; break;
-		default: console.log("Error: Current day is equal to: " + currentDay);
-	}
-	res.render("list", { kindOfDay: day });
+    let today = new Date();
+    var day = "";
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    var day = today.toLocaleDateString("en-US", options)
+
+    res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+
+    if (req.body.list === "WorkItem") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems })
 })
 
-app.listen(3000, function (req, res) {
-	console.log("Server is up and running!")
+app.get("/about", function(req, res) {
+    res.render("about");
 })
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
+
+app.listen(3000, function(req, res) {
+    console.log("Server is up and running!")
+})
+
+//earlier method of fetching the day and assinging to individual HTML pages.
+// var currentDay = today.getDay();
+
+// console.log(currentDay);
+// if (currentDay === 6 || currentDay === 0) {
+// 	day = "weekend";
+// 	// res.sendFile(__dirname + "/weekend.html");
+// } else {
+// 	day = "weekday";
+// 	// res.sendFile(__dirname + "/weekday.html");
+// }
+
+// switch (currentDay) {
+// 	case 0: day = "Sunday"; break;
+// 	case 1: day = "Monday"; break;
+// 	case 2: day = "Tuesday"; break;
+// 	case 3: day = "Wednesday"; break;
+// 	case 4: day = "Thursday"; break;
+// 	case 5: day = "Friday"; break;
+// 	case 6: day = "Saturday"; break;
+// 	default: console.log("Error: Current day is equal to: " + currentDay);
+// }
